f3a6c5d630c6a2a5cec2e92b1efac643e806f23f
GordoxGit <romain.begot@gmail.com>
2025-08-11 19:55:13 +0200

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    java
+}
+
+group = "com.hikabrain"
+version = "1.1.6"
+
+repositories {
+    mavenCentral()
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+}
+
+dependencies {
+    compileOnly("org.spigotmc:spigot-api:1.21-R0.1-SNAPSHOT")
+    testImplementation("junit:junit:4.13.2")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+tasks.withType<org.gradle.api.tasks.compile.JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+    options.release.set(21)
+}
