cde3fc398b344e9d55007b9784aa8c72ef2269c9
GordoxGit <romain.begot@gmail.com>
2025-09-21 16:56:03 +0200

    caca pipi

--- src/main/java/com/heneria/nexus/command/NexusCommand.java
+++ src/main/java/com/heneria/nexus/command/NexusCommand.java
@@ -11,7 +11,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 
 /**
- * Main command entry point.
+ * Main command entry point. caca
  */
 public final class NexusCommand implements CommandExecutor, TabCompleter {
 
